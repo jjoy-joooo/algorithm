80e36ae7b9c1b89de6213e6174e0005655cbaaa1
jjoy-joooo <jjoy.joooo@gmail.com>
2024-04-12 17:15:15 +0900

    [python] 백준 2947번 - 나무조각

---
+++ python/backjoon/2947.py
@@ -0,0 +1,9 @@
+# 나무 조각
+arr = list(map(int, input().split()))
+
+n = len(arr)
+for i in range(n):
+    for j in range(n - i - 1):
+        if arr[j] > arr[j + 1]:
+            arr[j], arr[j + 1] = arr[j + 1], arr[j]
+            print(*arr)
